1c0fe22eda778385a45c5e53bc12d38bb9aa97b1
Daniel <dsouzaborini@gmail.com>
2021-06-15 19:06:29 -0300

    add req 7

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import FetchApi from '../services/fetchApi';
 // import FetchApi from '../services/fetchApi';
+import '../App.css';
 
 class Questions extends React.Component {
   constructor() {
@@ -9,8 +10,11 @@ class Questions extends React.Component {
     this.state = {
       perguntas: [],
       indice: 2,
+      disable: false,
     };
     this.updtadeQuestions = this.updtadeQuestions.bind(this);
+    this.checkAnswer = this.checkAnswer.bind(this);
+    this.renderQuestions = this.renderQuestions.bind(this);
   }
 
   componentDidMount() {
@@ -21,27 +25,79 @@ class Questions extends React.Component {
     this.setState({ perguntas: perguntas.results });
   }
 
-  render() {
-    const { perguntas, indice } = this.state;
-    if (perguntas.length < 1) return <p>Carregando...</p>;
+  checkAnswer(event) {
+    const buttons = document.querySelectorAll('.default');
+    this.setState({
+      disable: true,
+    });
+
+    buttons.forEach((button) => {
+      if (button.name === 'correct-answer') {
+        return button.classList.add('green-border');
+      }
+      return button.classList.add('red-border');
+    });
+    // buttons[0].classList.remove('default');
+    // buttons[0].classList.add('red-border');
+    // console.log(buttons);
+  }
+
+  renderQuestions() {
+    const { perguntas, indice, disable } = this.state;
     return (
       <div>
         <p data-testid="question-category">{`Categoria ${perguntas[indice].category}`}</p>
         <p data-testid="question-text">{`Pergunta: ${perguntas[indice].question}`}</p>
-        <button type="button" data-testid="correct-answer">
+        <button
+          type="button"
+          data-testid="correct-answer"
+          name="correct-answer"
+          disabled={ disable }
+          className="default"
+          onClick={ this.checkAnswer }
+        >
           {`Resposta1: ${perguntas[indice].correct_answer}`}
         </button>
-        <button type="button" data-testid={ `wrong-answer-${0}` }>
+        <button
+          type="button"
+          data-testid={ `wrong-answer-${0}` }
+          name="wrong-answer"
+          disabled={ disable }
+          className="default"
+          onClick={ this.checkAnswer }
+        >
           {`Resposta2: ${perguntas[indice].incorrect_answers[0]}`}
         </button>
-        <button type="button" data-testid={ `wrong-answer-${1}` }>
+        <button
+          type="button"
+          data-testid={ `wrong-answer-${1}` }
+          name="wrong-answer"
+          disabled={ disable }
+          className="default"
+          onClick={ this.checkAnswer }
+        >
           {`Resposta3: ${perguntas[indice].incorrect_answers[1]}`}
         </button>
-        <button type="button" data-testid={ `wrong-answer-${2}` }>
+        <button
+          type="button"
+          data-testid={ `wrong-answer-${2}` }
+          name="wrong-answer"
+          disabled={ disable }
+          className="default"
+          onClick={ this.checkAnswer }
+        >
           {`Resposta4: ${perguntas[indice].incorrect_answers[2]}`}
         </button>
         <p>teste</p>
       </div>
+    );
+  }
+
+  render() {
+    const { perguntas } = this.state;
+    if (perguntas.length < 1) return <p>Carregando...</p>;
+    return (
+      this.renderQuestions()
     );
   }
 }
