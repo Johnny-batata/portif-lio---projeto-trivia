93eae0528a5f37a7518a5f5afc321e26a00ff583
Emerson Saturnino <programador.emerson@hotmail.com>
2021-06-15 16:16:54 -0300

    Requerement05

--- src/services/fetchImageGravatar.js
+++ src/services/fetchImageGravatar.js
@@ -2,10 +2,10 @@ import md5 from 'crypto-js/md5';
 
 export default async function FetchImageGravatar(email) {
   const hash = md5(email).toString();
-  console.log(hash);
+  // console.log(hash);
   const response = await fetch(`https://www.gravatar.com/avatar/${hash}`);
   const data = await response;
-  console.log(data.url);
+  // console.log(data.url);
 
   return data.url;
   // localStorage.setItem('token', data.token);
