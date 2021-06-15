189ac40ff9ed4bc12badef85ea5f09ef34af5117
Daniel <dsouzaborini@gmail.com>
2021-06-15 20:59:45 -0300

    req 8

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -9,16 +9,42 @@ class Questions extends React.Component {
 
     this.state = {
       perguntas: [],
-      indice: 2,
+      indice: 0,
       disable: false,
+      timer: 30,
     };
     this.updtadeQuestions = this.updtadeQuestions.bind(this);
     this.checkAnswer = this.checkAnswer.bind(this);
     this.renderQuestions = this.renderQuestions.bind(this);
+    this.timer = this.timer.bind(this);
+    this.nextQuestion = this.nextQuestion.bind(this);
+    this.clearStyles = this.clearStyles.bind(this);
   }
 
   componentDidMount() {
     FetchApi().then((data) => this.updtadeQuestions(data));
+    this.timer();
+  }
+
+  componentDidUpdate() {
+    const { timer } = this.state;
+    if (timer === 0) setTimeout(() => { this.nextQuestion(); }, 5000);
+    // if (timer === 0) this.nextQuestion();
+  }
+
+  nextQuestion() {
+    this.setState((prev) => ({ indice: prev.indice + 1 }));
+    this.setState(() => ({ timer: 30 }));
+    this.setState({ disable: false });
+    this.clearStyles();
+  }
+
+  clearStyles() {
+    const buttons = document.querySelectorAll('.default');
+    buttons.forEach((button) => {
+      button.classList.remove('green-border');
+      button.classList.remove('red-border');
+    });
   }
 
   updtadeQuestions(perguntas) {
@@ -37,13 +63,22 @@ class Questions extends React.Component {
       }
       return button.classList.add('red-border');
     });
-    // buttons[0].classList.remove('default');
-    // buttons[0].classList.add('red-border');
     // console.log(buttons);
   }
 
+  regress() {
+    return this.setState((prev) => ({ timer: prev.timer - 1 }));
+  }
+
+  timer() {
+    const { timer } = this.state;
+    const MS = 1000;
+    const intervalo = setInterval(() => { this.regress(); }, MS);
+    // if (timer === 0) return clearInterval(intervalo);
+  }
+
   renderQuestions() {
-    const { perguntas, indice, disable } = this.state;
+    const { perguntas, indice, disable, timer } = this.state;
     return (
       <div>
         <p data-testid="question-category">{`Categoria ${perguntas[indice].category}`}</p>
@@ -88,7 +123,7 @@ class Questions extends React.Component {
         >
           {`Resposta4: ${perguntas[indice].incorrect_answers[2]}`}
         </button>
-        <p>teste</p>
+        <p>{ timer }</p>
       </div>
     );
   }
