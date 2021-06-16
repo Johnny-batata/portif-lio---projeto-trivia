b186b3b1e4c4836e83ff793b4bab3177ae291b71
Daniel <dsouzaborini@gmail.com>
2021-06-16 17:10:10 -0300

    add req 12

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,11 +1,66 @@
 import React, { Component } from 'react';
+import FetchImageGravatar from '../services/fetchImageGravatar';
+import Question from '../components/Questions';
 
 class Feedback extends Component {
+  constructor() {
+    super();
+    this.state = {
+      url: '',
+      score: 0,
+      name: '',
+    };
+    this.updateState = this.updateState.bind(this);
+    this.header = this.header.bind(this);
+  }
+
+  componentDidMount() {
+    const name = JSON.parse(localStorage.getItem('state'));
+    // console.log(name);
+    this.updateState(name);
+  }
+
+  async updateState(name) {
+    await FetchImageGravatar(name.player.gravatarEmail)
+      .then((data) => this.setState({ url: data }));
+    this.setState({ score: name.player.score });
+    this.setState({ name: name.player.name });
+  }
+
+  header() {
+    const { url, score, name } = this.state;
+    return (
+      <header>
+        <img
+          src={ url }
+          alt="player.jpeg"
+          data-testid="header-profile-picture"
+        />
+        <p
+          data-testid="header-player-name"
+        >
+          {name}
+
+        </p>
+        <p
+          data-testid="header-score"
+
+        >
+          {score}
+
+        </p>
+      </header>
+    );
+  }
+
   render() {
     return (
-      <p data-testid="feedback-text">
-        feedback
-      </p>
+      <div>
+        <p data-testid="feedback-text">
+          feedback
+        </p>
+        { this.header() }
+      </div>
     );
   }
 }
