6fa398f4ee0095b2521f6a96f806a11f7881a60e
Johnny batata <johnny.arguello007@gmail.com>
2021-09-16 21:16:14 -0300

    Updates

---
+++ precache-manifest.032b4b72a2c4521c254edb8be3665105.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01379ef4a3d6d0a03ec4136abef8a0aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9ea18f83fa8695b5635",
+    "url": "/static/css/main.7970d83d.chunk.css"
+  },
+  {
+    "revision": "47ad67809d68cf4f2a6e",
+    "url": "/static/js/2.0387b4bd.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/static/js/2.0387b4bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9ea18f83fa8695b5635",
+    "url": "/static/js/main.084879c6.chunk.js"
+  },
+  {
+    "revision": "0f6ff4c0bace0555b202",
+    "url": "/static/js/runtime-main.0aacbbf5.js"
+  },
+  {
+    "revision": "06137282012b9ddba9ec49fde1e3ce00",
+    "url": "/static/media/ampulheta.06137282.gif"
+  },
+  {
+    "revision": "8db8598d07e68baea9ca8f6da559534e",
+    "url": "/static/media/backgound.8db8598d.jpg"
+  }
+]);
